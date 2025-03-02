d6e85085c7010e0281064ef45dd87df8075ffec2
Álvaro Andrés Romero Castro <alromeroca@unal.edu.co>
2025-03-02 11:11:29 -0500

    Front inicial de reservas
    Añadido layout general de reservas, funciónes sin terminar de añadido y remoción de espacios, y animaciónes

--- proyecto/client/src/App.js
+++ proyecto/client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginRegisterPage from './components/LoginRegisterPage';
 import PantallaPrincipal from './components/PantallaPrincipal';
 import MisReservas from './components/MisReservas';
+import Reservar from './components/Reservar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginRegisterPage />} />
         <Route path="/pantalla_principal" element={<PantallaPrincipal />} />
+        <Route path="/reservar" element={<Reservar />} />
         <Route path="/mis_reservas" element={<MisReservas />} />
       </Routes>
     </Router>
